6974d5ff85498f06800cc645aa0111e1bea187db
Tommi <tommi.savolainen79@gmail.com>
2024-05-03 10:07:21 +0300

    6.8 tehty

---
+++ src/components/AnecdoteForm.jsx
@@ -0,0 +1,27 @@
+import { useDispatch } from 'react-redux';
+import { createNew } from '../reducers/anecdoteReducer';
+
+const AnecdoteForm = () => {
+    const dispatch = useDispatch();
+
+    const createNewAnecdote = (event) => {
+        event.preventDefault();
+        const content = event.target.new.value;
+        event.target.new.value = '';
+        dispatch(createNew(content));
+    };
+
+    return (
+        <div>
+            <h2>create new</h2>
+            <form onSubmit={createNewAnecdote}>
+                <div>
+                    <input name="new" />
+                </div>
+                <button type="submit">create</button>
+            </form>
+        </div>
+    );
+};
+
+export default AnecdoteForm;
